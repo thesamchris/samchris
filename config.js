dc5fd1b619693f7af917bba3e3d9ad23750f95ea
Samuel Paul Christopher <thesamchris@gmail.com>
2019-05-08 02:38:25 +0800

    not working

---
+++ config.js
@@ -0,0 +1,4 @@
+export default {
+    apiKey: "AIzaSyCJMEEfBxQgbxKaAn1BV9sPlneYmBdXPnY",
+    spreadsheetId: "19MeCi4toNfM5ZYYuxfzOaIEMXRXG5TU7pHj-a5noGRo"
+}
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const About = () => (
+  <div>
+    <h1>Sam Chris About.</h1>
+    <Link to="/order">order</Link>
+  </div>
+)
+
+export default About
