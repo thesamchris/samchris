aea65d4ae9bf4072989944d912ab9c5ec3bbc93c
Samuel Paul Christopher <thesamchris@gmail.com>
2019-07-06 18:19:56 +0800

    initial commit version 2

--- config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    apiKey: "AIzaSyCJMEEfBxQgbxKaAn1BV9sPlneYmBdXPnY",
-    spreadsheetId: "19MeCi4toNfM5ZYYuxfzOaIEMXRXG5TU7pHj-a5noGRo"
-}
---
+++ website/src/components/header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './header.module.css'
+
+const Header = () => (
+    <header className={styles.header__container}>
+        <div className={styles.logo} />
+        <p>Website will be live by 4:00 p.m. (Brunei Time) on 7th July 2019</p>
+    </header>
+)
+
+export default Header
