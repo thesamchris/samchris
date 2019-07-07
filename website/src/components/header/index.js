13a4f5dc0963961fdedb4a5be29a0a170d90c4f6
Samuel Paul Christopher <thesamchris@gmail.com>
2019-07-07 15:00:37 +0800

    updated launch date

--- website/src/components/header/index.js
+++ website/src/components/header/index.js
@@ -4,7 +4,7 @@ import styles from './header.module.css'
 const Header = () => (
     <header className={styles.header__container}>
         <div className={styles.logo} />
-        <p>Website will be live by 4:00 p.m. (Brunei Time) on 7th July 2019</p>
+        <p>Website will be live by 4:00 p.m. (Brunei Time) on 13th July 2019</p>
     </header>
 )
 
